4159964ba4aab479b482e47d6c83a139bd8af545
Jangchaeyun <zzangsally@naver.com>
2023-11-05 22:27:56 +0900

    1105 HomeSection Post

--- frontend/src/Components/HomeSection/HomeSection.jsx
+++ frontend/src/Components/HomeSection/HomeSection.jsx
@@ -1,13 +1,19 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Button } from "@mui/material";
 import { useFormik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
+import ImageIcon from "@mui/icons-material/Image";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
 
 const validationSchema = Yup.object().shape({
   content: Yup.string().required("Starweet text is required"),
 });
 
 const HomeSection = () => {
+  const [uploadingImage, setUploadingImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+
   const handleSubmit = (values) => {
     console.log("values ", values);
   };
@@ -20,6 +26,14 @@ const HomeSection = () => {
     onSubmit: handleSubmit,
     validationSchema,
   });
+
+  const handleSelectImage = (event) => {
+    setUploadingImage(true);
+    const imgUrl = event.target.files[0];
+    formik.setFieldValue("image", imgUrl);
+    setSelectedImage(imgUrl);
+    setUploadingImage(false);
+  };
 
   return (
     <div className="space-y-5">
@@ -46,7 +60,39 @@ const HomeSection = () => {
                   <span className="text-red-500">{formik.errors.content}</span>
                 )}
               </div>
-              <div></div>
+              {/* <div>
+                <img src="" alt="" />
+              </div> */}
+              <div className="flex justify-between items-center mt-5">
+                <div className="flex space-x-5 items-center">
+                  <label className="flex items-center space-x-2 rounded-md cursor-pointer">
+                    <ImageIcon className="text-[#1d9bf0]" />
+                    <input
+                      type="file"
+                      name="imageFile"
+                      className="hidden"
+                      onChange={handleSelectImage}
+                    />
+                  </label>
+                  <FmdGoodIcon className="text-[#1d9bf0]" />
+                  <TagFacesIcon className="text-[#1d9bf0]" />
+                </div>
+
+                <div>
+                  <Button
+                    sx={{
+                      width: "100%",
+                      borderRadius: "20px",
+                      paddingY: "8px",
+                      paddingX: "20px",
+                      bgcolor: "#1e88e5",
+                    }}
+                    variant="contained"
+                  >
+                    Starweet
+                  </Button>
+                </div>
+              </div>
             </form>
           </div>
         </div>
