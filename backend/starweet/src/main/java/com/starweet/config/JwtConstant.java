390351d834f223275cf6bb96641ddbd98b0781e3
Jangchaeyun <zzangsally@naver.com>
2023-11-20 22:15:16 +0900

    1120 JWT TokenProvider

---
+++ backend/starweet/src/main/java/com/starweet/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.starweet.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "dhajqwbvgVewgfrvqehriwqrenerkjhweuirahshgqwvfrqwebrgyVwerkjwe";
+    public static final String JWT_HEADER = "Authorization";
+}
