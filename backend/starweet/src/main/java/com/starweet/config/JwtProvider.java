a0c725be153bb5f8c61fa4c03225a92d738e5f4e
Jangchaeyun <zzangsally@naver.com>
2023-11-22 22:19:12 +0900

    1122 User & Starweet Entity

--- backend/starweet/src/main/java/com/starweet/config/JwtProvider.java
+++ backend/starweet/src/main/java/com/starweet/config/JwtProvider.java
@@ -1,11 +1,35 @@
 package com.starweet.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 public class JwtProvider {
     SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
+    public String generateToken(Authentication auth) {
+        String jwt = Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 86400000))
+                .claim("email", auth.getName())
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
 
+        return jwt;
+    }
+
+    public String getEmailFromToken(String jwt) {
+        jwt = jwt.substring(7);
+
+        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(jwt).getBody();
+
+        String email = String.valueOf(claims.get("email"));
+
+        return email;
+    }
 }
---
+++ backend/starweet/src/main/java/com/starweet/model/Starweet.java
@@ -0,0 +1,36 @@
+package com.starweet.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Starweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private String content;
+
+    @OneToMany(mappedBy = "starweet", cascade = CascadeType.ALL)
+    private List<Like> likes = new ArrayList<>();
+
+    @OneToMany
+    private List<Starweet> replyStarweet = new ArrayList<>();
+
+    @ManyToMany
+    private List<User> restarweetUser = new ArrayList<>();
+
+    @ManyToOne
+    private Starweet replyFor;
+
+    private boolean isReply;
+    private boolean isStarweet;
+
+}
