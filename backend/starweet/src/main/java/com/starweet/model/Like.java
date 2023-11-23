634a6f415561544461c37448400b83151e29c604
Jangchaeyun <zzangsally@naver.com>
2023-11-23 22:01:31 +0900

    1123 Verification & UserRepository

--- backend/starweet/src/main/java/com/starweet/model/Like.java
+++ backend/starweet/src/main/java/com/starweet/model/Like.java
@@ -1,4 +1,20 @@
 package com.starweet.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "likes")
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Starweet starweet;
 }
