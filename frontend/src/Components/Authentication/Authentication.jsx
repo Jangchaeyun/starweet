d84cbe3b4f7933cba3fe1e626fbdbbf398bb83e3
Jangchaeyun <zzangsally@naver.com>
2023-12-08 22:34:42 +0900

    Authentication Front

--- frontend/src/Components/Authentication/Authentication.jsx
+++ frontend/src/Components/Authentication/Authentication.jsx
@@ -1,8 +1,13 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import { Grid, Button } from "@mui/material";
+import React, { useState } from "react";
 import { GoogleLogin } from "@react-oauth/google";
+import AuthModal from "./AuthModal";
 
 const Authentication = () => {
+  const [openAuthModel, setOpenAuthModel] = useState(false);
+  const handleOpenAuthModel = () => setOpenAuthModel(true);
+  const handleCloseAuthModel = () => setOpenAuthModel(false);
+
   return (
     <div>
       <Grid className="overflow-y-hidden" container>
@@ -55,16 +60,49 @@ const Authentication = () => {
           </div>
         </Grid>
         <Grid className="px-10" lg={5} xs={12}>
-          <h1 className="font-bold text-7xl">Happening Now</h1>
-          <h1 className="font-bold text-3xl py-16">Join Starweet Todaay</h1>
+          <h1 className="mt-40 font-bold text-7xl">Happening Now</h1>
+          <h1 className="font-bold text-3xl py-16">Join Starweet Today</h1>
           <div className="w-[60%]">
             <div className="w-full">
               <GoogleLogin width={330} />
               <p className="py-5 text-center">OR</p>
+              <Button
+                onClick={handleOpenAuthModel}
+                fullWidth
+                variant="contained"
+                size="large"
+                sx={{
+                  borderRadius: "29px",
+                  py: "7px",
+                }}
+              >
+                Create Account
+              </Button>
+
+              <p className="text-sm mt-2">
+                By signing up, you agree to the Terms of Service and Privacy
+                Policy, including Cookie Use.
+              </p>
+            </div>
+            <div className="mt-10">
+              <h1 className="font-bold text-xl mb-5">Already Have Account?</h1>
+              <Button
+                onClick={handleOpenAuthModel}
+                fullWidth
+                variant="outlined"
+                size="large"
+                sx={{
+                  borderRadius: "29px",
+                  py: "7px",
+                }}
+              >
+                login
+              </Button>
             </div>
           </div>
         </Grid>
       </Grid>
+      <AuthModal open={openAuthModel} handleClose={handleCloseAuthModel} />
     </div>
   );
 };
